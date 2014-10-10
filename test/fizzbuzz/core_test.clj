ee3bd11fa8fda5a16dd025bcb223e9f2c81b4d0a
Henry Stanley <henry@henrystanley.com>
2014-10-10 12:22:12 +0100

    updated test names to read more clearly

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -6,26 +6,27 @@
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
-(deftest is-divisible-by-three
+(deftest is-3-divisible-by-three
   (testing "3 is divisible by 3"
     (is (divisible-by-three? 3))))
 
-(deftest is-divisible-by-three
+(deftest is-5-divisible-by-three
   (testing "5 is not divisible by 3"
     (is (= false (divisible-by-three? 5)))))
 
-(deftest is-divisible-by-five
+(deftest is-5-divisible-by-five
   (testing "5 is divisible by 5"
     (is (divisible-by-five? 5))))
 
-(deftest is-divisible-by-five
+(deftest is-1-divisible-by-five
   (testing "1 is not divisible by 5"
     (is (= false (divisible-by-five? 1)))))
 
-(deftest is-divisible-by-fifteen
+(deftest is-15-divisible-by-fifteen
   (testing "15 is divisible by 15"
     (is (= (divisible-by-five? 15) (divisible-by-three? 15)) )))
 
-(deftest is-divisible-by-fifteen
+(deftest is-1-divisible-by-fifteen
   (testing "1 is divisible by 15"
     (is (= false (divisible-by-five? 1) (divisible-by-three? 1)) )))
+
